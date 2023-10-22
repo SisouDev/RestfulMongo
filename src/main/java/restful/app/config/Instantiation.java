6ae196698cccbeb0d5792b5766fb2dca1dffe6e0
Karolina <karolinasingulani2@gmail.com>
2023-10-22 19:53:35 -0300

    Entity post

--- src/main/java/restful/app/config/Instantiation.java
+++ src/main/java/restful/app/config/Instantiation.java
@@ -1,12 +1,16 @@
 package restful.app.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import restful.app.domain.Post;
 import restful.app.domain.User;
+import restful.app.repository.PostRepository;
 import restful.app.repository.UserRepository;
 
 @Configuration
@@ -15,26 +19,30 @@ public class Instantiation implements CommandLineRunner{
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PostRepository postRepository;
+
     @Override
     public void run(String... args) throws Exception {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
         userRepository.deleteAll();
+        postRepository.deleteAll();
+
         User u1 = new User(null, "Maria Brown", "maria@gmail.com");
         User u2 = new User(null, "Alex Green", "alex@gmail.com");
         User u3 = new User(null, "Bob Grey", "bob@gmail.com");
         User u4 = new User(null, "Afonso Luis", "afonsolindo@gmail.com");
         User u5 = new User(null, "Sarah Luisa", "sarahgatinha@gmail.com");
         User u6 = new User(null, "Nelio Alves", "acenelio@gmail.com");
-        User u7 = new User(null, "David Jones", "david@gmail.com");
-        User u8 = new User(null, "Jennifer White", "jennifer@gmail.com");
-        User u9 = new User(null, "William Miller", "william@gmail.com");
-        User u10 = new User(null, "Linda Garcia", "linda@gmail.com");
-        User u11 = new User(null, "James Rodriguez", "james@gmail.com");
-        User u12 = new User(null, "Patricia Martinez", "patricia@gmail.com");
-        User u13 = new User(null, "Charles Hernandez", "charles@gmail.com");
-        User u14 = new User(null, "Karen Gonzalez", "karen@gmail.com");
-        User u15 = new User(null, "Thomas Wilson", "thomas@gmail.com");
+        
+        Post post1 = new Post(null, sdf.parse("21/03/2023"), "Vacations", "I am going to travel, see you later!", u1);
+        Post post2 = new Post(null, sdf.parse("23/03/2023"), "Good morning.", "Im happy today!", u1);
 
-        userRepository.saveAll(Arrays.asList(u1, u2, u3, u4, u5, u6, u7, u8, u9, u10, u11, u12, u13, u14, u15));
+        userRepository.saveAll(Arrays.asList(u1, u2, u3, u4, u5, u6));
+        postRepository.saveAll(Arrays.asList(post1, post2));
 
     }
     
---
+++ src/main/java/restful/app/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package restful.app.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import restful.app.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+    
+}
