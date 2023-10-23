38ffeb63665a7dfd1e7280cc3b594cdde8fa99ac
Karolina <karolinasingulani2@gmail.com>
2023-10-23 15:10:30 -0300

    @query

--- src/main/java/restful/app/repository/PostRepository.java
+++ src/main/java/restful/app/repository/PostRepository.java
@@ -3,11 +3,16 @@ package restful.app.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import restful.app.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String title);
+
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
