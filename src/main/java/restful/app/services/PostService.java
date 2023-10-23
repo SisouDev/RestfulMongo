206c0bef4ca2cf9819dac1d38f7c396204b29dca
Karolina <karolinasingulani2@gmail.com>
2023-10-23 15:02:25 -0300

    query methods

--- src/main/java/restful/app/services/PostService.java
+++ src/main/java/restful/app/services/PostService.java
@@ -1,5 +1,6 @@
 package restful.app.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,4 +20,7 @@ public class PostService {
         return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
     }
 
+    public List<Post> findByTitle(String text){
+        return repository.findByTitleContainingIgnoreCase(text);
+    }
 }
